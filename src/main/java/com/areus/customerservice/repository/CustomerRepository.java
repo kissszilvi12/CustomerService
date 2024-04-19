75711b3e858297e3eebeeb6a708828fda7b79491
szkis <szkis@iakos.com>
2024-04-19 16:42:44 +0200

    Optimizes imports

--- src/main/java/com/areus/customerservice/repository/CustomerRepository.java
+++ src/main/java/com/areus/customerservice/repository/CustomerRepository.java
@@ -4,11 +4,9 @@ import com.areus.customerservice.model.Customer;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
-import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
