e5314aa3f854f88a6f6a387e564028f905af1d63
szkis <szkis@iakos.com>
2024-04-23 22:01:53 +0200

    Adds authentication with username and password

--- src/main/java/com/areus/customerservice/config/SecurityConfig.java
+++ src/main/java/com/areus/customerservice/config/SecurityConfig.java
@@ -1,4 +1,40 @@
 package com.areus.customerservice.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/", "/login").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .logout(LogoutConfigurer::permitAll);
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.builder()
+                    .username("user")
+                    .password("password")
+                    .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
 }
